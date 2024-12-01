2951bb9d04fe748e854d2473de3636b4d51d21f1
ncsmikewoods <mike.woods@ncontracts.com>
2024-12-01 09:26:01 -0600

    day 1 part 1

--- AdventOfCode/Day01/Solver.cs
+++ AdventOfCode/Day01/Solver.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Microsoft.VisualBasic;
 
 namespace Day01;
 
 public class Solver
 {
-    private List<int> _totals;
+    private List<int> _left;
+    private List<int> _right;
 
     public Solver()
     {
@@ -16,7 +18,13 @@ public class Solver
 
     public int Solve1()
     {
-        return 1;
+        _left = _left.Order().ToList();
+        _right = _right.Order().ToList();
+
+        var combined = _left.Zip(_right);
+
+        var sum = combined.Sum(pair => Math.Abs(pair.First - pair.Second));
+        return sum;
     }
 
     public int Solve2()
@@ -27,6 +35,16 @@ public class Solver
 
     void GetInputs()
     {
+        var lines = File.ReadAllLines("input.txt");
+
+        _left = new List<int>();
+        _right = new List<int>();
         
+        foreach (var line in lines)
+        {
+            var tokens = line.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            _left.Add(int.Parse(tokens[0]));
+            _right.Add(int.Parse(tokens[1]));
+        }
     }
 }
