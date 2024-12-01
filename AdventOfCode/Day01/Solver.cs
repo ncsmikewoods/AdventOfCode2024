2031225e79d9e29141c8c22e557f730e9dc5df39
ncsmikewoods <mike.woods@ncontracts.com>
2024-12-01 09:08:59 -0600

    initial project structure

---
+++ AdventOfCode/Day01/Solver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day01;
+
+public class Solver
+{
+    private List<int> _totals;
+
+    public Solver()
+    {
+        GetInputs();
+    }
+
+    public int Solve1()
+    {
+        return 1;
+    }
+
+    public int Solve2()
+    {
+        return 2;
+    }
+
+
+    void GetInputs()
+    {
+        
+    }
+}
