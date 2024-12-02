ca6b9b8accaebc0eab3bbb75045780781fa443cd
ncsmikewoods <mike.woods@ncontracts.com>
2024-12-02 17:05:41 -0600

    Day 2 part 1

---
+++ Day02/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Day02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var solver = new Solver();
+
+        Part1(solver);
+        Part2(solver);
+    }
+
+    static void Part1(Solver solver)
+    {
+        Console.WriteLine("Solving Part 1...");
+        var start = DateTime.Now;
+
+        var result = solver.Solve1();
+        var duration = DateTime.Now - start;
+
+        Console.WriteLine($"Solution 1: {result}");
+        Console.WriteLine($"Duration: {Math.Round(duration.TotalMilliseconds)}ms");
+        Console.WriteLine("");
+    }
+
+    static void Part2(Solver solver)
+    {
+        Console.WriteLine("Solving Part 2...");
+        var start = DateTime.Now;
+        
+        var result = solver.Solve2();
+        var duration = DateTime.Now - start;
+        
+        Console.WriteLine($"Solution 2: {result}");
+        Console.WriteLine($"Duration: {Math.Round(duration.TotalMilliseconds)}ms");
+        Console.WriteLine("");
+    }
+}
---
+++ Day02/Solver.cs
@@ -0,0 +1,76 @@
+﻿namespace Day02;
+
+public class Solver
+{
+    private List<List<int>> _reports;
+
+    public Solver()
+    {
+        GetInputs();
+    }
+
+    public int Solve1()
+    {
+        var fails = _reports
+            .Where(report => !IsSafe(report))
+            .ToList();
+
+        // foreach (var fail in fails)
+        // {
+        //     Console.WriteLine($"Fail: {string.Join(" ", fail)}");
+        // }
+        
+        return _reports.Count - fails.Count;
+    }
+
+    public int Solve2()
+    {
+        return 1;
+    }
+
+    bool IsSafe(List<int> report)
+    {
+        var isAscending = true;
+
+        for (var i = 1; i < report.Count; i++)
+        {
+            var prev = report[i - 1];
+            var curr = report[i];
+            
+            var didIncrease = curr > prev;
+            var diff = Math.Abs(prev - curr);
+            
+            // Rule 1
+            if (i == 1)
+            {
+                isAscending = didIncrease;
+            }
+            else
+            {
+                if (didIncrease != isAscending)
+                {
+                    return false;
+                }
+            }
+            
+            // Rule 2
+            if (diff is < 1 or > 3)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    void GetInputs()
+    {
+        _reports = 
+            File.ReadAllLines("input.txt")
+                .Select(line => 
+                    line.Split(' ')
+                        .Select(int.Parse)
+                        .ToList())
+                .ToList();
+    }
+}
