47e987eaca9aa6927c6b76df277c850e5682b0d4
ncsmikewoods <mike.woods@ncontracts.com>
2024-12-02 23:13:05 -0600

    Day 3 Part 1

---
+++ Day03/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Day03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var solver = new Solver();
+
+        RunIt("Part 1", () => solver.Solve1().ToString());
+        RunIt("Part 2", () => solver.Solve2().ToString());
+    }
+    
+    static void RunIt(string name, Func<string> solveFunc)
+    {
+        Console.WriteLine($"Solving {name}...");
+        var start = DateTime.Now;
+        
+        var result = solveFunc();
+        var duration = DateTime.Now - start;
+
+        Console.WriteLine($"Solution: {result}");
+        Console.WriteLine($"Duration: {Math.Round(duration.TotalMilliseconds)}ms");
+        Console.WriteLine("");
+    }
+}
---
+++ Day03/Solver.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day03;
+
+public class Solver
+{
+    private List<Instruction> _instructions;
+
+    public Solver()
+    {
+        GetInputs();
+    }
+
+    public double Solve1()
+    {
+        return _instructions.Sum(x => x.Multiply());
+    }
+
+    public int Solve2()
+    {
+        return 1;
+    }
+
+    
+    void GetInputs()
+    {
+        var input = File.ReadAllText("input.txt");
+        var regex = new Regex(@"mul\(\d{1,3},\d{1,3}\)");
+        
+        var matches = regex.Matches(input).Select(x => x.Value).ToList();
+        _instructions = matches.Select(m => new Instruction(m)).ToList();
+    }
+}
+
+public class Instruction
+{
+    public Instruction(string operation)
+    {
+        var tokens = operation.Split(["mul", ",", "(", ")"], StringSplitOptions.RemoveEmptyEntries);
+
+        Operation = operation;
+        Left = int.Parse(tokens[0]);
+        Right = int.Parse(tokens[1]);
+    }
+    
+    public string Operation { get; set; }
+    public int Left { get; set; }
+    public int Right { get; set; }
+
+    public double Multiply()
+    {
+        return Left * Right;
+    }
+}
