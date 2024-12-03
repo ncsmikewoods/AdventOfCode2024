21869d893f740db1411348ad91b9c69c38c100df
ncsmikewoods <mike.woods@ncontracts.com>
2024-12-02 18:11:00 -0600

    Clean up

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -6,32 +6,19 @@ class Program
     {
         var solver = new Solver();
 
-        Part1(solver);
-        Part2(solver);
+        RunIt("Part 1", () => solver.Solve1().ToString());
+        RunIt("Part 2", () => solver.Solve2().ToString());
     }
-
-    static void Part1(Solver solver)
+    
+    static void RunIt(string name, Func<string> solveFunc)
     {
-        Console.WriteLine("Solving Part 1...");
-        var start = DateTime.Now;
-
-        var result = solver.Solve1();
-        var duration = DateTime.Now - start;
-
-        Console.WriteLine($"Solution 1: {result}");
-        Console.WriteLine($"Duration: {Math.Round(duration.TotalMilliseconds)}ms");
-        Console.WriteLine("");
-    }
-
-    static void Part2(Solver solver)
-    {
-        Console.WriteLine("Solving Part 2...");
+        Console.WriteLine($"Solving {name}...");
         var start = DateTime.Now;
         
-        var result = solver.Solve2();
+        var result = solveFunc();
         var duration = DateTime.Now - start;
-        
-        Console.WriteLine($"Solution 2: {result}");
+
+        Console.WriteLine($"Solution: {result}");
         Console.WriteLine($"Duration: {Math.Round(duration.TotalMilliseconds)}ms");
         Console.WriteLine("");
     }
