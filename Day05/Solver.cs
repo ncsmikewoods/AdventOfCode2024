0ecbb9e56803bbeb52c72ad524bade2437cfeb7a
ncsmikewoods <mike.woods@ncontracts.com>
2024-12-04 22:54:16 -0600

    Day 5 scaffold

---
+++ Day05/Solver.cs
@@ -0,0 +1,24 @@
+﻿namespace Day05;
+
+public class Solver
+{
+    public Solver()
+    {
+        GetInputs();
+    }
+    
+    public int Solve1()
+    {
+        return 1;
+    }
+
+    public int Solve2()
+    {
+        return 1;
+    }
+
+    void GetInputs()
+    {
+        var lines = File.ReadAllLines("input.txt");
+    }
+}
